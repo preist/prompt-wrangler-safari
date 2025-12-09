08ca32b9e08ce61c5af692656478996b2ad98d81
Igor Putina <iputina@mixbook.com>
2025-12-09 17:49:17 +0200

    Initial commit

---
+++ Prompt Wrangler/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Prompt Wrangler
+//
+//  Created by Igor Putina on 09.12.2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ Prompt WranglerTests/Prompt_WranglerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Prompt_WranglerTests.swift
+//  Prompt WranglerTests
+//
+//  Created by Igor Putina on 09.12.2025.
+//
+
+import Testing
+@testable import Prompt_Wrangler
+
+struct Prompt_WranglerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
